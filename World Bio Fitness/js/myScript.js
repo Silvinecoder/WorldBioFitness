c00dcfdfdeaae39407f91e42660685cf5fd053c3
Silvinecoder <82526116+Silvinecoder@users.noreply.github.com>
2021-06-07 14:07:00 +0100

    Delete World Bio Fitness directory

--- World Bio Fitness/js/myScript.js
+++
@@ -1,6 +0,0 @@
-/* Background Images */
-function nextSlide() {
-	if (typeof $ == "undefined") var $ = function(sel) { return document.querySelector(sel); }
-	$(".bk-img").appendChild($(".bk-img img:first-child"));
-}
-setInterval(nextSlide, 3000);
